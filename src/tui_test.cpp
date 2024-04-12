7287127e4676fa234b69c868f4dfe6fe268ccc1d
LeonardoBriseno22310185 <160084527+LeonardoBriseno22310185@users.noreply.github.com>
2024-04-12 01:58:35 +0000

    Version 1

---
+++ src/tui_test.cpp
@@ -0,0 +1,33 @@
+
+#include<ftxui/dom/elements.hpp>
+#include<ftxui/screen/screen.hpp>
+#include<iostream>
+#include<string>
+
+using namespace std;
+using namespace ftxui;
+
+
+int main(int argc, char const *argv[])
+{
+    const std::string texto = "Hola mundo";
+    Element textElement = text(texto);
+    
+    Element dibujo = hbox(textElement | border);
+
+    Dimension Alto = Dimension::fixed(10);
+    Dimension Ancho = Dimension::fixed(10);
+
+    Screen pantalla = Screen::Create(Ancho,Alto);
+
+    Render(pantalla,dibujo);
+
+    pantalla.Print();
+    cout<<endl;
+
+
+
+
+
+    return 0;
+}
